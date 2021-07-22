40a0ccb5c082fe1595496671e86d1c885b1e5979
taazi1101 <48558222+taazi1101@users.noreply.github.com>
2021-07-22 16:27:28 +0300

    Add files via upload

--- time.py
+++ time.py
@@ -7,7 +7,7 @@ from datetime import date
 sys.argv.pop(0)
 
 if "-h" in sys.argv:
-    print("Add time zones as arguments\nFormat: Area/Location/region\nExamples: europe/heslinki america/canada/toronto")
+    print("Add time zones as arguments\nFormat: Area/Location/region\nExamples: europe/heslinki America/Argentina/Salta\nMore help at: http://worldtimeapi.org")
     exit()
 
 def get(zone):
@@ -19,11 +19,14 @@ def get(zone):
 t = time.localtime()
 sys_time = str(date.today()) + "--" + time.strftime("%H:%M:%S", t)
 print("System time: " + sys_time)
+try:
+    ipb = dict(requests.get("http://worldtimeapi.org/api/ip").json()).get("datetime")
+    ipbl = ipb.split(".")
+    print("Ip based time: " + ipbl[0].replace("T","--"))
+except:
+    print("Failed to grap ip based time")
 for arg in sys.argv:
     resp = dict(get(arg))
     retime = str(resp.get("datetime"))
     splitt = retime.split(".")[0]
     print(resp.get("timezone") + ": " + splitt.replace("T","--"))
-
-
-    
